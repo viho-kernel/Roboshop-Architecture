d803a669cebe84332926a8ff7c3b088cf938509e
viho-kernel <vihari.reddy1802@gmail.com>
2026-03-10 00:53:20 +0530

    latest commit

---
+++ 40-Databases/iam.tf
@@ -0,0 +1,42 @@
+resource "aws_iam_role" "mysql" {
+
+    name = "mysql-role"
+
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-Mysql"
+    }
+  )
+  
+}
+
+resource "aws_iam_policy" "mysql" {
+  name        = "${var.project}-${var.environment}-Mysql-Policy"
+  description = "A policy for MySQL Ec2 instance"
+  policy      = templatefile("mysql-iam-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "mysql" {
+  role       = aws_iam_role.mysql.name
+  policy_arn = aws_iam_policy.mysql.arn
+}
+
+resource "aws_iam_instance_profile" "mysql" {
+  name = "${var.project}-${var.environment}-mysql"
+  role = aws_iam_role.mysql.name
+}
