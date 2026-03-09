46cca22317571af34a0e03db2b88c1fe28d2a6a3
viho-kernel <vihari.reddy1802@gmail.com>
2026-03-10 01:03:41 +0530

    latest commit

--- 40-Databases/iam.tf
+++ 40-Databases/iam.tf
@@ -28,7 +28,7 @@ resource "aws_iam_role" "mysql" {
 resource "aws_iam_policy" "mysql" {
   name        = "${var.project}-${var.environment}-Mysql-Policy"
   description = "A policy for MySQL Ec2 instance"
-  policy      = templatefile("mysql-iam-policy.json", 
+  policy      = templatefile("aws_iam_policy.json", 
   {
     environment=var.environment
   })
