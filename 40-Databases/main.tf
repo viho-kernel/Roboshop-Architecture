247671a1e3658e51339f2d1cf6048bfa43a582c2
viho-kernel <vihari.reddy1802@gmail.com>
2026-03-10 03:38:43 +0530

    latest commit

--- 40-Databases/main.tf
+++ 40-Databases/main.tf
@@ -38,6 +38,46 @@ resource "terraform_data" "mongodb" {
   }
 }
 
+resource "aws_instance" "redis" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    subnet_id = local.database_subnet_id
+    vpc_security_group_ids = [local.redis]
+    
+
+    tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-redis"
+        }
+    )
+}
+
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.redis.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh" # Local file path
+    destination = "/tmp/bootstrap.sh"    # Destination path on the remote machine
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh redis"
+    ]
+  }
+}
+
 resource "aws_instance" "mysql" {
     ami = local.ami_id
     instance_type = "t3.micro"
