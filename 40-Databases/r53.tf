febc76a3943fa06ee63e224975e5b13bba825d57
viho-kernel <vihari.reddy1802@gmail.com>
2026-03-10 03:40:03 +0530

    latest commit

--- 40-Databases/r53.tf
+++ 40-Databases/r53.tf
@@ -15,3 +15,12 @@ resource "aws_route53_record" "mysql" {
   records = [aws_instance.mysql.private_ip]
   allow_overwrite = true
 }
+
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = "1"
+  records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
+}
