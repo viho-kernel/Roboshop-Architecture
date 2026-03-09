e394b7d963ea2379d8f936b77218471adeb74009
viho-kernel <vihari.reddy1802@gmail.com>
2026-03-10 00:11:53 +0530

    latest commit

--- 40-Databases/r53.tf
+++ 40-Databases/r53.tf
@@ -6,3 +6,12 @@ resource "aws_route53_record" "mongodb" {
   records = [aws_instance.mongodb.private_ip]
   allow_overwrite = true
 }
+
+resource "aws_route53_record" "mysql" {
+  zone_id = var.zone_id
+  name    = "mysql-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = "1"
+  records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
+}
